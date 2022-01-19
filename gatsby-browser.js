d7a3fde141539806817a42d75d9210f2cde1a1e3
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-01-19 20:45:53 +0530

    The following changes were done:
      - added a global styles file for tailwindcss styles
      - added a Layout file in components directory that wraps around each page
      - added a gatsby-browser.js file that imports global styles and Layout wrapper
      - created a wrapPageElement to wrap layout around each page

---
+++ gatsby-browser.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Layout from "./src/components/Layout";
+import "./src/styles/global.css";
+
+export const wrapPageElement = ({ element }) => {
+  return <Layout>{element}</Layout>;
+};
---
+++ src/components/Layout.tsx
@@ -0,0 +1,53 @@
+import React, { FC, useState } from "react";
+import CartSVG from "./svgs/Cart";
+import CloseSVG from "./svgs/Close";
+import MenuSVG from "./svgs/Menu";
+
+const Layout: FC = ({ children }) => {
+  const [isClosed, setIsClosed] = useState(true);
+  return (
+    <>
+      <header className="bg-orange-500 w-full overflow-visible absolute">
+        <nav className="container mx-auto py-2 px-3 flex justify-between items-start lg:items-center">
+          <div
+            onClick={() => {
+              setIsClosed(!isClosed);
+            }}
+            className="lg:hidden"
+          >
+            <MenuSVG
+              className={isClosed ? "h-8 w-8 stroke-slate-50" : "hidden"}
+            />
+            <CloseSVG
+              className={isClosed ? "hidden" : "h-8 w-8 stroke-slate-50"}
+            />
+          </div>
+          <ul className="list-none flex flex-col lg:flex-row justify-between lg:flex-grow lg:mr-2 items-baseline ">
+            <li
+              className={`${
+                isClosed ? "hidden" : ""
+              } lg:block bg-green-400 mt-2 lg:mt-0 order-2 lg:order1`}
+            >
+              1
+            </li>
+            <li className="bg-green-400 order-1 lg:order-2">2</li>
+            <li
+              className={`${
+                isClosed ? "hidden" : ""
+              } lg:block bg-green-400 mt-2 lg:mt-0 order-3 lg:order-3`}
+            >
+              3
+            </li>
+          </ul>
+          <div className="flex items-center">
+            <CartSVG className="fill-slate-50 h-8 w-8" />
+            <span className="text-base text-slate-50">1</span>
+          </div>
+        </nav>
+      </header>
+      {children}
+    </>
+  );
+};
+
+export default Layout;
