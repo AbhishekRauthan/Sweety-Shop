37c501ae3f5346ec25006b10f1491d6609fbe556
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-01-19 16:40:10 +0530

    added tailwindcss to project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-      title: `sweety`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `sweety`,
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-react-helmet"]
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-react-helmet"],
 };
