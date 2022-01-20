eb93edf6c045e3a1fc6100da005ad166a5475bf4
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-01-20 13:24:48 +0530

    corrected import path for Layout

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from "./src/components/Layout";
+import Layout from "./src/components/layout/index";
 import "./src/styles/global.css";
 
 export const wrapPageElement = ({ element }) => {
